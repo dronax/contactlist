56966c5cc047ccaa7ef4981501df62f5c3a6ea3c
ablution-6837 <poudelabhishake@gmail.com>
2022-08-01 19:53:25 +0545

    secondcommit

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,21 @@ import Header from "./Header";
 import Addcontact from "./Addcontact";
 import Contactlist from "./Contactlist";
 function App() {
-  const LOCAL_STORAGE_KEY = "contacts";
+  const LOCAL_STORAGE_KEY = "contactss";
   const [contactss, setContacts] = useState([]);
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contactss, contact]);
   };
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contactss));
-  }, [contactss])
   useEffect(() => {
     const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if (retriveContacts) setContacts(retriveContacts);
-  },[])
+  }, []);
+  useEffect(() => {
+    if(contactss.length>0)
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contactss));
+  }, [contactss]);
+
   return (
     <div className="ui container">
       <Header />
